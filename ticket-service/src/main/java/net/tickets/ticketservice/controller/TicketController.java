036198adf0a7d5f4c1f6a661c2dbfabff75569f3
andrei_davidenko <davidenkav@gmail.com>
2025-07-04 12:24:24 +0500

    run from Idea, RestTemplate

--- ticket-service/src/main/java/net/tickets/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/net/tickets/ticketservice/controller/TicketController.java
@@ -3,6 +3,7 @@ package net.tickets.ticketservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -10,8 +11,18 @@ import java.util.Map;
 @RequestMapping("/tickets")
 public class TicketController {
 
+    private final RestTemplate restTemplate;
+
+    public TicketController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
     @GetMapping("/test")
     public Map<String, String> test() {
-        return Map.of("message", "ticket ok");
+        String eventMessage = restTemplate.getForObject("http://localhost:8088/api/events/test", String.class);
+        return Map.of(
+                "message", "ticket ok",
+                "eventMessage", eventMessage != null ? eventMessage : "No response from event service");
     }
 }
