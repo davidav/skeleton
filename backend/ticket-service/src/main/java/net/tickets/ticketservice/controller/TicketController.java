8ca3ae144c689faeb6d9c95fdd156754cf8b1ac8
andrei_davidenko <davidenkav@gmail.com>
2025-07-04 15:15:57 +0500

    new structure, added Spring profiles - local, docker

--- backend/ticket-service/src/main/java/net/tickets/ticketservice/controller/TicketController.java
+++ backend/ticket-service/src/main/java/net/tickets/ticketservice/controller/TicketController.java
@@ -1,5 +1,7 @@
 package net.tickets.ticketservice.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,9 @@ import java.util.Map;
 @RequestMapping("/tickets")
 public class TicketController {
 
+    @Value("${gateway.url}")
+    private String gatewayUrl;
+
     private final RestTemplate restTemplate;
 
     public TicketController(RestTemplate restTemplate) {
@@ -20,7 +25,7 @@ public class TicketController {
 
     @GetMapping("/test")
     public Map<String, String> test() {
-        String eventMessage = restTemplate.getForObject("http://localhost:8088/api/events/test", String.class);
+        String eventMessage = restTemplate.getForObject(gatewayUrl + "/api/events/test", String.class);
         return Map.of(
                 "message", "ticket ok",
                 "eventMessage", eventMessage != null ? eventMessage : "No response from event service");
